903fa3433258cf59c2a16d602fad77779a6bac2b
Pr0curo <jhelbig86@gmail.com>
2016-10-09 10:56:40 +0200

    remove luacpp and add sol2

--- nepensebla/src/main.cpp
+++ nepensebla/src/main.cpp
@@ -7,6 +7,8 @@
 
 #include <stdio.h>
 
+#include "../../sol2/sol.hpp"
+
 // int main(int argc, const char *argv[])
 int main()
 {
@@ -15,6 +17,20 @@ int main()
     NepenForm fm(std::move(nep));
 
     fm.show();
+
+    //--
+    // sol::state lua;
+
+    // int x=0;
+    // lua.set_function("beep", [&x]{std::cout << "add one" << ++x << "\n";});
+    // lua.script("beep()");
+
+    // assert(x==1);
+
+    // sol::function b = lua["beep"];
+    // b();
+    // assert(x==2);
+    //--
 
     nana::exec();
 
